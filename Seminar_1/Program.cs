b461cefd79376cceea11dd2c02bbff71f1f35b2e
Shamil <kurbanov@outlook.de>
2023-08-02 14:32:18 +0200

    add "task_2.cs"

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Seminar_1.task_2;	
+
+namespace Seminar_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionMax(0, 0);
+        }
+    }
+}
---
+++ Seminar_1/task_2.cs
@@ -0,0 +1,49 @@
+//class task_2
+using static System.Console;
+using System;
+
+/*
+ * Write a program that takes two numbers as input and outputs which number is greater and which is less.
+*/
+namespace Seminar_1
+{
+    class task_2
+    {
+        public static void FunctionMax( int a, int b)
+        {
+            WriteLine();
+            WriteLine("# <Task 2> #");
+            WriteLine("Write a program that takes two numbers as input and outputs which number is greater and which is less.");
+            WriteLine("-----------------------------------------");
+            //input first number
+            Write("Input first number: ");
+            a = Convert.ToInt32(ReadLine());
+            //input second number
+            Write("Input second number: ");
+            b = Convert.ToInt32(ReadLine());
+            //check which number is greater and which is less
+            if (a > b)
+            {
+                WriteLine("First number is greater than second number");
+                WriteLine("--> {0} > {1}", a, b);
+            }
+            else if (a < b)
+            {
+                WriteLine("First number is less than second number");
+                WriteLine("--> {0} < {1}", a, b);	
+            }
+            else
+            {
+                WriteLine("First number is equal to second number");
+                WriteLine("--> {0} = {1}", a, b);
+            }
+            WriteLine();
+            WriteLine("-----------------------------------------");
+            WriteLine();
+
+
+
+
+        }
+    }
+}
