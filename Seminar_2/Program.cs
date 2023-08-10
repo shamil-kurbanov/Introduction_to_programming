e6abe32d514c70ecb1aa41e44dc0732f607c79af
Shamil <kurbanov@outlook.de>
2023-08-10 13:47:58 +0200

    Update Program file

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿//Write a program that takes a three-digit number as input and outputs the second digit of that number
+//on the console.
+
+using System;
+using static System.Console;
+using static Seminar_2.Task_10_GetSecondDigit;
+
+
+namespace Seminar_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSecondDigit(0);
+            
+            WriteLine("Want you other three-digit number to be calculated?");
+            WriteLine("Enter 'y' for yes or 'n' for no: ");
+            string answer = ReadLine(); 
+            
+            switch (answer)
+            {
+                case "y":
+                    GetSecondDigit(0);
+                    break;
+                case "n":
+                    WriteLine("Good bye!");
+                    break;
+                default:
+                    WriteLine("Invalid input!");
+                    break;
+            }
+
+        }
+    }
+}
