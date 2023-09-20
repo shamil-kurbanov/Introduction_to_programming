8549608a018dee280cba9c5a3c7b43fc2f97c49d
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-09-20 16:25:57 +0200

    Add function FuctionPrintNaturalNumbersFromNtoOne that all natural numbers in the range from N to 1

---
+++ Seminar_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+using static Seminar_9.Task_64;
+//using static Seminar_9.Task_66;
+//using static Seminar_9.Task_68;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        prompt();
+        WriteLine("--------------------");
+
+        WriteLine("--------------------");
+
+    }
+}
---
+++ Seminar_9/Task_64.cs
@@ -0,0 +1,49 @@
+/*
+Set the value to N. 
+Write a program that prints all natural numbers in the range from N to 1. 
+Execute using recursion.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+using System;
+using static System.Console;
+
+namespace Seminar_9;
+
+public class Task_64
+{
+//FuctionPrintNaturalNumbersFromNtoOne
+    public static void prompt() //promp
+    {
+        Write("Enter N: ");
+        int n = int.Parse(ReadLine());
+
+        if (n >= 1)
+        {
+            Write("Natural numbers from " + n + " to 1: ");
+            fuctionPrintNaturalNumbersFromNtoOne(n);
+            WriteLine();
+        }else{
+            WriteLine("N must be greater than 0");
+        }
+    }
+
+    public static void fuctionPrintNaturalNumbersFromNtoOne(int n)
+    {
+        if (n >= 1)
+        {
+            Write(n);
+            if (n != 1)
+            {
+                Write(", ");
+            }
+        fuctionPrintNaturalNumbersFromNtoOne(n - 1); //recursion call
+        }
+        
+    }
+
+
+
+}
